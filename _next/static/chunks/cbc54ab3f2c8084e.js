67ff4038726ef7d037205b2e53babf9076b4c9d0
nas23ise <nas23ise@gmail.com>
2025-11-23 18:01:30 +0530

    Updates

---
+++ _next/static/chunks/cbc54ab3f2c8084e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/86955c44a6fbb00c.js",
+  "static/chunks/turbopack-329ce62538319e9b.js"
+])
---
+++ _next/static/chunks/f7ca8bc3cdbd81bd.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/86955c44a6fbb00c.js",
+  "static/chunks/turbopack-91c32be306b33eb9.js"
+])
